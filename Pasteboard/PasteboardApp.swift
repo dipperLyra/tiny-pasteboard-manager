73dd308e9395d7051211199fd06a6f559b8dd150
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-06-18 14:31:40 +0100

    Initial Commit

---
+++ Pasteboard/PasteboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteboardApp.swift
+//  Pasteboard
+//
+//  Created by Eche on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PasteboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
