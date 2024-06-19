382e09ec840285aadab61ff729a27c8fac430121
brakioz <31044786+dipperLyra@users.noreply.github.com>
2024-06-19 13:53:13 +0100

    add timer to pastboard content checking

--- Pasteboard/ContentView.swift
+++ Pasteboard/ContentView.swift
@@ -12,26 +12,69 @@ import Foundation
 var store: [String] = []
 
 struct ContentView: View {
+    @State private var items: [Item] = dataBuilder()
+    @State private var timer: Timer?
     
     var body: some View {
         VStack {
             Text("Paste Board Manager")
                 .font(.largeTitle)
                 .padding()
-            .padding()
-            Table(dataBuilder()) {
+            Table(items) {
                 TableColumn("S/N") { item in
                     Text("\(item.column1 + 1)")
                 }
                 TableColumn("content", value: \.column2)
                 TableColumn("Action") { item in
                     Button("Copy") {
-                        print("Copied")
+                        writeSysPb(item.column2)
                     }
                 }
             }
         }
         .padding()
+        .onAppear {
+            startTimer()
+        }
+        .onDisappear {
+            stopTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { _ in
+            checkPasteboard()
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func checkPasteboard() {
+        let curPbContent = readSysPb()
+        let isPbContentChanged = store.last?.lowercased() != curPbContent.lowercased()
+        
+        if !curPbContent.isEmpty && isPbContentChanged {
+            store.append(curPbContent)
+            items = dataBuilder()
+        }
+    }
+    
+    func readSysPb() -> String {
+        var content = ""
+        if let str = NSPasteboard.general.string(forType: .string) {
+            content = str
+        }
+
+        return content
+    }
+    
+    func writeSysPb(_ content: String) {
+        let pastboard = NSPasteboard.general
+        pastboard.clearContents()
+        pastboard.setString(content, forType: .string)
     }
 }
 
@@ -41,24 +84,8 @@ struct Item: Identifiable {
     let column2: String
 }
 
-func readSysPb() -> String {
-    var content = ""
-    if let str = NSPasteboard.general.string(forType: .string) {
-        content = str
-    }
-
-    return content
-}
-
 func dataBuilder() -> Array<Item> {
     var data: [Item] = []
-    var isPastebinChanged: Bool
-    
-    isPastebinChanged = store.last?.lowercased() != readSysPb().lowercased()
-    
-    if (!readSysPb().isEmpty && isPastebinChanged) {
-        store.append(readSysPb())
-    }
     
     for (index, item) in store.reversed().enumerated() {
         data.append(Item(column1: index, column2: item))
---
+++ README.md
@@ -0,0 +1,3 @@
+#  MacOs Pasteboard Manager
+
+This is a tiny pasteboard manager to enable me view and copy previously copied items.
